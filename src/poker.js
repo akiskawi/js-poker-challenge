d473b6a8cbdf57458b1c84f77dbf4d31cca867f6
John Poikilidis <akiskawi@gmail.com>
2023-04-13 10:30:39 +0300

    main test passed

--- src/poker.js
+++ src/poker.js
@@ -1,11 +1,49 @@
-function winningPair() {}
+const letterValues = {
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14
+}
+function isValidPair(pair) {
+  if (getNumber(pair[0]) === getNumber(pair[1])) {
+    return pair
+  }
+  return [-1]
+}
+function getNumber(num) {
+  let returningValue = num
+  const letters = Object.keys(letterValues)
+  if (letters.includes(num)) {
+    returningValue = letterValues[num]
+  }
+  return returningValue
+}
+function getWinnerPair(pairOne, pairTwo) {
+  if (getNumber(pairOne[0]) >= getNumber(pairTwo[0])) {
+    return pairOne
+  }
+  return pairTwo
+}
+
+function winningPair(pairOne, pairTwo) {
+  const winner = getWinnerPair(isValidPair(pairOne), isValidPair(pairTwo))
+  if (winner[0] === -1) {
+    // console.log(winner)
+    return []
+  }
+  // console.log('here')
+  // console.log(winner[0])
+  return winner
+  // return getWinnerPair(pairOne, pairTwo)
+}
 
 // Extension criteria
 
 function winningPairFromArray() {}
 
 function winning3CardHand() {}
-
+// winningPair(['J', 'Q'], ['3', '7'])
+// winningPair(['3', '5'], ['3', '4'])
 module.exports = {
   winningPair,
   winningPairFromArray,
